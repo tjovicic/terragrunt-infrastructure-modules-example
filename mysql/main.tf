a54c8f78778669599fc62c5d34b2bf949d8336e5
Tihomir Jovicic <tihomir.jovicic.develop@gmail.com>
2020-11-12 17:17:27 +0100

    update terraform version to 0.13.5 for mysql example

--- mysql/main.tf
+++ mysql/main.tf
@@ -9,7 +9,7 @@
 terraform {
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
   # The latest version of Terragrunt (v0.25.1 and above) recommends Terraform 0.13.3 or above.
-  required_version = "= 0.13.3"
+  required_version = "= 0.13.5"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
